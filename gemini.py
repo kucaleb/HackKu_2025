bbb0c5630bd72ea823ec81870fc35bb35b6b21d4
kucaleb <caleb.neil@ku.edu>
2025-04-05 16:06:47 -0500

    made Gemini output a dictionary

--- gemini.py
+++ gemini.py
@@ -3,6 +3,7 @@ from google.genai import types
 
 import PIL.Image
 import os
+import json
 
 image = PIL.Image.open('checkers_page_3_example.jpg')
 
@@ -13,4 +14,8 @@ response = client.models.generate_content(
     contents=["You are a chef doing meal prep for a healthy resteraunt. The image will containt coupons from a local store. Your job is to create three healthy meals from some of the items in the coupon and return the meals in the following JSON format. {Meal1 Name: meal1 name, Meal Items: {Item1: Quantity,...}Meal2 Name: meal2 name...} but instead of writing ... finish the meals. The meals are only inspired from the items in the coupons, so please write out a full list of ingredients, even if the ingredients aren't in the coupons.", image]
 )
 
-print(response.text)
+text = response.text
+meals = json.loads(text.removeprefix("```json\n").removesuffix("```"))
+
+print(meals)
+print(type(meals))
