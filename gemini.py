97d386ad99a5bfc4dfc9e6febbd2b2011552c686
kucaleb <caleb.neil@ku.edu>
2025-04-05 15:32:56 -0500

    added gemini support

---
+++ gemini.py
@@ -0,0 +1,16 @@
+from google import genai
+from google.genai import types
+
+import PIL.Image
+import os
+
+image = PIL.Image.open('checkers_page_3_example.jpg')
+
+client = genai.Client(api_key=os.environ.get('GEMINI_API_KEY'))
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=["You are a chef doing meal prep for a healthy resteraunt. The image will containt coupons from a local store. Your job is to create three healthy meals from some of the items in the coupon and return the meals in the following JSON format. {Meal1 Name: meal1 name, Meal Items: {Item1: Quantity,...}Meal2 Name: meal2 name...} but instead of writing ... finish the meals. The meals are only inspired from the items in the coupons, so please write out a full list of ingredients, even if the ingredients aren't in the coupons.", image]
+)
+
+print(response.text)
